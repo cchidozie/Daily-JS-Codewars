3d3641d5fd2e13a1931bc0aa210702ce2608d04e
chidozie chukwugbo <chukwugboc@gmail.com>
2022-12-27 22:38:56 +0100

    Dec 27 Optimization

--- gradeBook.js
+++ gradeBook.js
@@ -3,11 +3,28 @@ function getGrade(s1, s2, s3) {
   let score = (s1 + s2 + s3) / 3
   console.log(score)
 
-  if (score >= 90) console.log('A')
-  else if (score >= 80) console.log('B')
-  else if (score >= 70) console.log('C')
-  else if (score >= 60) console.log('D')
-  else console.log('E')
+  switch (true) {
+    case (score >= 90) :
+      // return 'A'
+      console.log('A')
+      break
+    case (score <= 80) :
+      console.log('B')
+      // return 'B'
+      break
+    case (score >= 70):
+      console.log('C')
+      // return 'C'
+      break
+    case (score >= 60):
+      console.log('D')
+      // return 'D'
+      break
+    default:
+      // code block
+      console.log('E')
+    // return 'E'
+  }
 }
 
-getGrade(100, 60, 0)
+getGrade(100, 90, 10)
